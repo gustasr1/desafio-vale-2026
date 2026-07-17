9cba7735f225892505bdcb8153aa73a52e1dd002
Gustavo Santiago Rosa <gudaogustavo@gmail.com>
2026-07-17 20:25:23 -0300

    feat: filtro de caminhoes na base de analise apos eda

--- src/01_EDA.py
+++ src/01_EDA.py
@@ -4,11 +4,6 @@ import numpy as np
 
 telem_jan = pd.read_parquet('../data/telemetria/telemetry_jan.parquet')
 print(telem_jan.columns.tolist())
-# %%
-
-telem_jan.columns = telem_jan.columns.str.upper()
-telem_jan.columns = telem_jan.columns.str.strip()
-# %%
 
 telem_jan.duplicated().sum()
 # %%
@@ -97,3 +92,82 @@ telemetria.head()
 
 
 # %%
+
+telemetria.isnull().sum()
+
+# %%
+(telemetria['Is_Dont_Go'].value_counts(normalize=True) * 100).round(2)
+
+# %%
+telemetria['Is_Dont_Go'].value_counts()
+telemetria.head()
+
+# %% [markdown]
+
+#**Após checagem dos dados será feito um merge para avançar no EDA**
+
+# %%
+
+telemetria.sort_values(by='Data_Evento', inplace=True)
+apontamentos.sort_values(by='INICIO', inplace=True)
+
+#Padronização do tempo para nanossegundos para chaves de cruzamento
+telemetria['Data_Evento'] = telemetria['Data_Evento'].astype('datetime64[ns]')
+apontamentos['INICIO'] = apontamentos['INICIO'].astype('datetime64[ns]')
+apontamentos['FIM'] = apontamentos['FIM'].astype('datetime64[ns]')
+
+df_unificado = pd.merge_asof(
+    left=telemetria,
+    right=apontamentos,
+    by='TAG',
+    left_on='Data_Evento',
+    right_on='INICIO',
+    direction='backward'
+)
+
+# Se o FIM for nulo (NaT), consideramos como erro de cruzamento.
+filtro_dentro_do_ciclo = (df_unificado['Data_Evento'] <= df_unificado['FIM']) & (df_unificado['FIM'].notna())
+df_analise_final = df_unificado[filtro_dentro_do_ciclo].copy()
+
+df_eventos_orfaos = df_unificado[~filtro_dentro_do_ciclo]
+
+print(f"Total de registros unificados (Dentro de um ciclo): {len(df_analise_final)}")
+print(f"Total de eventos órfãos (Fora de ciclo): {len(df_eventos_orfaos)}")
+# %% [markdown]
+
+# Criando algumas visualizações antes do modelo preditivo
+
+# %%
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+analise_tipo = df_analise_final.groupby('TIPO').agg(
+    Total_Registros=('Is_Dont_Go', 'count'),
+    Total_Alertas=('Is_Dont_Go', 'sum')
+).reset_index()
+
+analise_tipo['Taxa_Falha_Percentual'] = (analise_tipo['Total_Alertas'] / analise_tipo['Total_Registros']) * 100
+
+analise_tipo = analise_tipo.sort_values(by='Taxa_Falha_Percentual', ascending=False)
+
+print(analise_tipo)
+
+plt.figure(figsize=(10, 6))
+sns.barplot(
+    data=analise_tipo, 
+    x='Taxa_Falha_Percentual', 
+    y='TIPO',
+    palette='Reds_r'
+)
+
+plt.title('Taxa de Alertas "Don\'t Go" por Tipo de Equipamento', fontsize=14, pad=15)
+plt.xlabel('Taxa de Falha (%)', fontsize=12)
+plt.ylabel('Tipo de Equipamento', fontsize=12)
+plt.grid(axis='x', linestyle='--', alpha=0.7)
+
+plt.tight_layout()
+plt.show()
+
+# %%
+df_analise_final['TIPO'].value_counts()
